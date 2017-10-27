d6c5095f617976ff81555476b977fe736cb48330
Milton Arango <milton.arango@codescrum.com>
2017-10-27 14:26:02 -0500

    Section 12, Lecture 110 The Require Auth Plug

---
+++ web/controllers/plugs/require_auth.ex
@@ -0,0 +1,23 @@
+defmodule Discuss.Plugs.RequireAuth do
+    @moduledoc """
+    Handles authorization permissons for the users. To control which actions they can perform.
+    """
+    import Plug.Conn
+    import Phoenix.Controller
+
+    def init(_params) do
+        # Blank initialization method
+    end
+
+    def call(conn, _params) do
+        if conn.assigns[:user] do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in")
+            |> redirect(to: Helpers.topic_path(conn, :index))
+            |> halt()
+        end 
+    end
+
+end
